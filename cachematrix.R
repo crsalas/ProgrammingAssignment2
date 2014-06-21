7b9cef1bb268b23bcc4850ea62d02c625da72fc7
crsalas <carlos.r.salas@gmail.com>
2014-06-20 20:51:28 -0500

    Final version of Programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 
-## The following contain functions to invert a matrix. If the  
+## The following script contains functions to invert a matrix. If the  
 ## inverse matrix has already been calculated, it retrieves it 
 ## from the cache. Otherwise, it inverts it and returns the value. 
 
@@ -10,24 +10,22 @@
 ## matrix. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
         mat  = NULL # initialize empty object "mat"
         
         # assigns "y" object to caches
-        set <- function(y) {
+        set = function(y) {
                 x <<- y # caches "y" value
                 mat <<- NULL # resets/caches mat to NULL
         }
         
         # retrieves values from matrix
-        get <- function() x 
-        
+        get = function() x 
         
         # calculate inverse matrix
-        set.invmat <- function(solve) mat <<- solve 
+        set.invmat = function(solve) mat <<- solve 
         
         # retrieve inverse matrix
-        get.invmat <- function() mat
+        get.invmat = function() mat
         
         # return list of functions
         list(set = set, get = get, 
@@ -37,12 +35,89 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This functions checks whether or not the matrix inverse has 
-## already been cached. If it has, then it returns the cached values 
-## (i.e., skipping the inversion calc). Otherwise, it calculates the 
-## inverse matrix and returns the values. 
-
+## The cacheSolve function checks whether or not the inverted matrix 
+## has already been cached. If it has, then it returns the cached values 
+## (i.e., skips the inversion calc). Otherwise, it calculates the 
+## inverse matrix and print it. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        mat = x$get.invmat() # retrieves inverted matrix (if available)
+
+        # if "mat" is NOT empty (i.e., value(s) are cached) ...
+        if(!is.null(mat)) { 
+                message("Retrieving cached matrix") 
+                return(mat) # return cached data (i.e.,skip computation)
+        }
+
+        # if it's NOT cached... 
+        data <- x$get() # retrieve the input matrix 
+        mat <- solve(data, ...)  # invert it
+        x$set.invmat(mat) # cache it for future use
+        mat # print the inverted matrix
 }
+
+
+
+
+#######################################################
+###                                                 ###
+###              FOR FUNCTION TESTING.              ###
+###        Coursera evaluators can ignore this.     ###
+###                                                 ###
+#######################################################
+
+## Scott Francis'example tests (THANKS!!!)
+        # create cacheable matrix object
+        m <- makeCacheMatrix( )
+        
+        # initailize with a an easy to inspect matrix 
+        m$set( matrix( c(0, 2, 2, 0 ), 2, 2))
+        
+        # note use of parens to retrieve the matrix part of the object
+        m$get()
+        
+        # test the inverse cacher
+        cacheSolve( m )
+        
+        # and again... should be cached now
+        cacheSolve( m )
+        
+        # product of matrix mult should be identity matrix AND we should get the cached message
+        m$get() %*% cacheSolve(m)
+        
+        # let R test identify for us
+        all.equal( diag(2), m$get() %*% cacheSolve(m) )
+        
+        # save the inverse off and let's see if we can break it...
+        m1<- cacheSolve(m)
+        
+        # set m to some new values
+        m$set( matrix( rnorm(4), 2, 2) )
+        
+        # does fetching the inverse without any pre-caching work?
+        all.equal( diag(2), m$get() %*% cacheSolve(m) )
+        
+        # what about testing m x the inverse we squirreled away in m1?
+        all.equal( diag(2), m$get() %*% m1 )
+
+        
+        # try a bigger matrix and see if we can notice the caching effects
+        m$set( matrix( rnorm( 1000000 ), 1000, 1000 ) )
+        system.time(cacheSolve(m)) # benchmarking
+        # First run:
+        # user  system elapsed 
+        # 2.01    0.00    2.03 
+
+        # Now w/ cache
+        system.time(cacheSolve(m)) 
+        # user  system elapsed 
+        # 0       0       0 
+        
+        # and double check it all still works...
+        all.equal( diag( 1000 ), m$get() %*% cacheSolve(m) )
+        
+        
+        
+        
+        
+        
