2c33bbc5a0ad875fc5d5b3e72d8ff09591081543
crsalas <carlos.r.salas@gmail.com>
2014-06-20 20:02:35 -0500

    Drafted the 'makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,36 @@
 ## from the cache. Otherwise, it inverts it and returns the value. 
 
 
-## This first function creates a special matrix that contains a list to
-## set/get the value of the matrix, and then set/get the value of the 
-## inverted matrix. 
-
+## This first function contains a list of four sub-functions that 
+## create a special matrix. These assign/retrieve the values of the 
+## input matrix, and then assign/retrieve the value of the inverted 
+## matrix. 
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        mat  = NULL # initialize empty object "mat"
+        
+        # assigns "y" object to caches
+        set <- function(y) {
+                x <<- y # caches "y" value
+                mat <<- NULL # resets/caches mat to NULL
+        }
+        
+        # retrieves values from matrix
+        get <- function() x 
+        
+        
+        # calculate inverse matrix
+        set.invmat <- function(solve) mat <<- solve 
+        
+        # retrieve inverse matrix
+        get.invmat <- function() mat
+        
+        # return list of functions
+        list(set = set, get = get, 
+             set.invmat = set.invmat,
+             get.invmat = get.invmat)
+                
 }
 
 
